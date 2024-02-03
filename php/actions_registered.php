5778e126b805a7dc4cd66e165c545b3b54984385
Giordano de Brito <39205592+GIOdeBrito@users.noreply.github.com>
2024-02-03 01:58:04 +0000

    Naming conventions and structuring

--- php/actions_registered.php
+++ php/actions_registered.php
@@ -6,24 +6,24 @@ $registered_actions = array
 (
     'gettime' => function ()
     {
-        require_once './services/function_get_time.php';
+        require_once './services/get_time.php';
         get_time();
     },
     
     'dumppost' => function () use ($action, $args)
     {
-        require_once './services/function_dump_post.php';
+        require_once './services/dump_post.php';
     },
     
     'getmessage' => function ()
     {
-        require_once './services/function_getmessage.php';
+        require_once './services/getmessage.php';
         requested_message();
     },
 
     'getsession' => function ()
     {
-        require_once './services/function_session_key.php';
+        require_once './services/session_key.php';
         get_session_keys();
     },
 );
